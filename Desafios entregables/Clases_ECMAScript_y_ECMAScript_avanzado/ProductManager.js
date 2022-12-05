19370cc7838f1051c18bcf23a764d05b0ed37422
Ezequiel Coletta <62360076+ecoletta@users.noreply.github.com>
2022-12-05 15:02:47 -0300

    Add files via upload
    Se realizan modificaciones en el entregable ECMAScript

--- Desafios entregables/Clases_ECMAScript_y_ECMAScript_avanzado/ProductManager.js
+++ Desafios entregables/Clases_ECMAScript_y_ECMAScript_avanzado/ProductManager.js
@@ -8,28 +8,38 @@ getProductos(){
     return this.products
 }
 
-agregarProducto(title, description, price, codigo = 0, thumbnail, stock){
+agregarProducto(title, description, price, codigo, idCodigo = 0, thumbnail, stock){
     let product = {
         title,
         description,
         price,
-        codigo: codigo || 0,
+        codigo,
+        idCodigo: idCodigo || 0,
         thumbnail,
         stock
         };
+
+    const found = this.products.some(item => item.codigo === product.codigo)
+    
+    if (!found){
+        console.log("Se agrego el producto correctamente")
+    }else{
+        console.log("Error, el codigo ingresado se encuentra repetido")
+        return
+    }
     
     if (this.products.length === 0){
-        product["codigo"] = 1;
+        product["idCodigo"] = 1;
     }else{
-        product["codigo"] = this.products[this.products.length - 1]["codigo"] + 1;
+        product["idCodigo"] = this.products[this.products.length - 1]["idCodigo"] + 1;
     }
 
     this.products.push(product)
 
     }
 
-    getProductById(codigo){
-        let product = this.products.filter((item) => item.codigo === codigo);
+    getProductById(idCodigo){
+        let product = this.products.filter((item) => item.idCodigo === idCodigo);
         if (product.length > 0){
             console.log(product)
             return product
@@ -42,7 +52,7 @@ agregarProducto(title, description, price, codigo = 0, thumbnail, stock){
 
 ///////////////////////////////////////////
 const productos = new ProductManager();
-productos.agregarProducto("Zelda Ocarina","Juego Nintendo 64",500,0,"Imagen",10);
-productos.agregarProducto("God of war","Juego PS4",500,0,"Imagen",10);
+productos.agregarProducto("Zelda Ocarina","Juego Nintendo 64",500,"codigo1",0,"Imagen",10);
+productos.agregarProducto("God of war","Juego PS4",500,"codigo2",0,"Imagen",10);
 //console.log(productos.getProductos());
 productos.getProductById(2);
